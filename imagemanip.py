abcbf3307292b042e0b16626dc33c4fb36552af4
ziro <palembani@gmail.com>
2020-11-27 11:22:39 +0700

    + Simplified invert

--- imagemanip.py
+++ imagemanip.py
@@ -20,18 +20,9 @@ def save_image(img: Image) -> BytesIO:
     return image_bytes
 
 def invert(img: bytes):
-    img = bytes2image(img)
-    img = img.resize((400, 400), 1)
-    if img.mode == "RGBA":
-        r,g,b,a = img.split()
-        rgb_img = Image.merge("RGB", (r,g,b))
-        inverted = ImageOps.invert(rgb_img)
-        r2,g2,b2 = inverted.split()
-        final = Image.merge('RGBA', (r2,g2,b2,a))
-    else:
-        im = img.resize((400, 400), 1)
-        final = ImageOps.invert(im)
-    return save_image(final)
+    img = bytes2image(img).resize((400, 400), 1)
+    img = img.convert("RGB")
+    return save_image(ImageOps.invert(img))
 
 def red(img: bytes):
     img = bytes2image(img)
